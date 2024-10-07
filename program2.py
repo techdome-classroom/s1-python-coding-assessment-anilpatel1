c1bb2892ee9d961acbb326f9b01c8eae899c90d9
Anil Patel <121279909+anilpatel1@users.noreply.github.com>
2024-10-07 12:28:13 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,18 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(message, pattern):
+    m, n = len(message), len(pattern)
+    dp = [[False] * (n + 1) for _ in range(m + 1)]
+    
+    dp[0][0] = True
 
-# write your code here
-  
-        return False
+    for j in range(1, n + 1):
+        if pattern[j - 1] == '*':
+            dp[0][j] = dp[0][j - 1]
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if pattern[j - 1] == '*':
+                dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+            elif pattern[j - 1] == '?' or pattern[j - 1] == message[i - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+
+    return dp[m][n]
